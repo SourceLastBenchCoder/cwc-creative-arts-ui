efa7b5621678f49a0875acbeaf0830c1730eae92
Chakrapani Upadhyaya <source.lastbenchcoder@gmail.com>
2022-09-17 18:11:39 +0530

    git push -u origin main

---
+++ src/components/shared/Header.js
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function Header() {
+  return (
+    <Navbar>
+    <Container>
+      <Navbar.Brand href="#home">
+      <img src="assets/logo.png" alt="" width={316}/>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <Navbar.Text>
+          Signed in as: <a href="#login">Mark Otto</a>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  );
+}
+
+export default Header;
